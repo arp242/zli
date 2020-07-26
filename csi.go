9b05a9fdb5bd0c1991779019b3dcfaa7d0e04de8
Martin Tournoij <martin@arp242.net>
2020-07-26 16:01:21 +0800

    Add ReplaceLine() and ReplaceLinef()
    Useful helpers.

--- csi.go
+++ csi.go
@@ -1,5 +1,19 @@
 package zli
 
+import "fmt"
+
 // ErasesLine erases the entire line and puts the cursor at the start of the
 // line.
 func EraseLine() { Stdout.Write([]byte("\x1b[2K\r")) }
+
+// ReplaceLine replaces the current line.
+func ReplaceLine(a ...interface{}) {
+	EraseLine()
+	fmt.Print(a...)
+}
+
+// ReplaceLinef replaces the current line.
+func ReplaceLinef(s string, a ...interface{}) {
+	EraseLine()
+	fmt.Printf(s, a...)
+}
